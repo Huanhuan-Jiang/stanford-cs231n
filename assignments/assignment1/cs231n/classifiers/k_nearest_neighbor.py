8268dbf01cfa02b8598493292070ca8e796251aa
Huanhuan Jiang <33575549+Huanhuan-Jiang@users.noreply.github.com>
2024-09-05 13:48:56 +0800

    Finish assignment 1 (#7)
    * Add assignment1 folder
    
    * Finish assignment 1 question 2 on linear_svm
    
    Finish assignment 1 question 2
    
    * Add gitignore
    
    * Finish assignment 1 question 3 on softmax (#2)
    
    * Finish assignment 1 question 4 on two layer net (#3)
    
    * Finish assignment 1 question 5 on features (#4)
    
    * Finish assignment 1

---
+++ assignments/assignment1/cs231n/classifiers/k_nearest_neighbor.py
@@ -0,0 +1,60 @@
+from builtins import range
+from builtins import object
+import numpy as np
+from past.builtins import xrange
+
+
+class KNearestNeighbor(object):
+    """ a kNN classifier with L2 distance """
+
+    def __init__(self):
+        pass
+
+    def train(self, X, y):
+        self.X_train = X
+        self.y_train = y
+
+    def predict(self, X, k=1, num_loops=0):
+        if num_loops == 0:
+            dists = self.compute_distances_no_loops(X)
+        elif num_loops == 1:
+            dists = self.compute_distances_one_loop(X)
+        elif num_loops == 2:
+            dists = self.compute_distances_two_loops(X)
+        else:
+            raise ValueError("Invalid value %d for num_loops" % num_loops)
+
+        return self.predict_labels(dists, k=k)
+
+    def compute_distances_two_loops(self, X):
+        num_test = X.shape[0]
+        num_train = self.X_train.shape[0]
+        dists = np.zeros((num_test, num_train))
+        for i in range(num_test):
+            for j in range(num_train):
+                dists[i, j] = np.sqrt(np.sum(np.square(self.X_train[j,:] - X[i, :])))
+        return dists
+
+    def compute_distances_one_loop(self, X):
+        num_test = X.shape[0]
+        num_train = self.X_train.shape[0]
+        dists = np.zeros((num_test, num_train))
+        for i in range(num_test):
+            dists[i, :] = np.sqrt(np.sum(np.square(self.X_train - X[i, :]), axis=1))
+        return dists
+
+    def compute_distances_no_loops(self, X):
+        num_test = X.shape[0]
+        num_train = self.X_train.shape[0]
+        dists = np.zeros((num_test, num_train))
+        dists = np.sqrt(np.sum(np.square(self.X_train - X[:, np.newaxis, :] ), axis=2) )
+        return dists
+
+    def predict_labels(self, dists, k=1):
+        num_test = dists.shape[0]
+        y_pred = np.zeros(num_test)
+        for i in range(num_test):
+            closest_y=self.y_train[np.argsort(dists[i, :])[:k]]
+            unique_labels, counts = np.unique(closest_y, return_counts=True)
+            y_pred[i] = unique_labels[np.argmax(counts)]
+        return y_pred
